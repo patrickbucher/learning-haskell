62f6ef1f67792da68635a528e732913c14b8416d
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-21 22:09:01 +0200

    formatted code

--- programming-in-haskell/ex-ch07.hs
+++ programming-in-haskell/ex-ch07.hs
@@ -1,7 +1,7 @@
 -- 7.1
 f x = x * 2
 p x = x `mod` 2 == 0
-xs = [0,1,2,3,4,5]
+xs = [0, 1, 2, 3, 4, 5]
 ys = map f (filter p xs)
 
 -- 7.2
@@ -13,13 +13,13 @@ any' p xs = foldr (||) False (map p xs)
 
 takeWhile' :: (a -> Bool) -> [a] -> [a]
 takeWhile' p [] = []
-takeWhile' p (x:xs) | p x = [x] ++ takeWhile' p xs
-                    | otherwise = []
+takeWhile' p (x : xs) | p x       = [x] ++ takeWhile' p xs
+                      | otherwise = []
 
 dropWhile' :: (a -> Bool) -> [a] -> [a]
 dropWhile' p [] = []
-dropWhile' p (x:xs) | p x = dropWhile p xs
-                    | otherwise = xs
+dropWhile' p (x : xs) | p x       = dropWhile p xs
+                      | otherwise = xs
 
 -- 7.3
 map' :: (a -> b) -> [a] -> [b]
@@ -34,14 +34,14 @@ dec2int :: [Int] -> Int
 dec2int xs = foldl (\acc -> \x -> acc * 10 + x) 0 xs
 
 -- 7.5
-curry' :: ((a,b) -> c) -> (a -> b -> c)
-curry' f = \x -> \y -> f (x,y)
+curry' :: ((a, b) -> c) -> (a -> b -> c)
+curry' f = \x -> \y -> f (x, y)
 
-uncurry' :: (a -> b -> c) -> ((a,b) -> c)
-uncurry' f = \(x,y) -> f x y
+uncurry' :: (a -> b -> c) -> ((a, b) -> c)
+uncurry' f = \(x, y) -> f x y
 
 -- 7.6
-unfold p h t x | p x = []
+unfold p h t x | p x       = []
                | otherwise = h x : unfold p h t (t x)
 
 int2bin = unfold (== 0) (`mod` 2) (`div` 2)
