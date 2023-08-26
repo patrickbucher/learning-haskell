7202b79eb19af1e316bfe23c58b551f3c820c6a4
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-26 18:28:34 +0200

    chapter 8: exercise 8

--- programming-in-haskell/ex-ch08.hs
+++ programming-in-haskell/ex-ch08.hs
@@ -60,3 +60,91 @@ halve xs  = (take n xs, drop n xs) where
 balance :: [a] -> BinaryTree a
 balance [x] = BinaryLeaf x
 balance xs  = BinaryNode (balance l) (balance r) where (l, r) = halve xs
+
+-- 8.5
+data Expr = Val Int | Add Expr Expr
+
+folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
+folde f g e = case e of
+  Val n   -> f n
+  Add a b -> g (folde f g a) (folde f g b)
+
+-- 8.6
+eval' :: Expr -> Int
+eval' e = folde id (+) e
+
+size :: Expr -> Int
+size e = folde (\_ -> 1) (+) e
+
+-- 8.7
+data Vielleicht a = Nichts | Nur a
+instance Eq a => Eq (Vielleicht a) where
+  Nichts == Nichts = True
+  Nichts == Nur _  = False
+  Nur _  == Nichts = False
+  Nur a  == Nur b  = a == b
+
+-- 8.8
+data Prop = Const Bool
+          | Var Char
+          | Not Prop
+          | And Prop Prop
+          | Imply Prop Prop
+          | Or Prop Prop
+          | Equiv Prop Prop
+
+type Assoc k v = [(k, v)]
+type Subst = Assoc Char Bool
+
+find :: Eq k => k -> Assoc k v -> v
+find k t = head [ v | (k', v) <- t, k == k' ]
+
+eval :: Subst -> Prop -> Bool
+eval _ (Const b  ) = b
+eval s (Var   x  ) = find x s
+eval s (Not   p  ) = not (eval s p)
+eval s (And   p q) = eval s p && eval s q
+eval s (Imply p q) = eval s p <= eval s q
+eval s (Or    p q) = eval s p || eval s q
+eval s (Equiv p q) = eval s p == eval s q
+
+vars :: Prop -> [Char]
+vars (Const _  ) = []
+vars (Var   x  ) = [x]
+vars (Not   p  ) = vars p
+vars (And   p q) = vars p ++ vars q
+vars (Imply p q) = vars p ++ vars q
+vars (Or    p q) = vars p ++ vars q
+vars (Equiv p q) = vars p ++ vars q
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False :) bss ++ map (True :) bss where bss = bools (n - 1)
+
+substs :: Prop -> [Subst]
+substs p = map (zip vs) (bools (length vs)) where vs = rmdups (vars p)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups []       = []
+rmdups (x : xs) = x : filter (/= x) (rmdups xs)
+
+isTaut :: Prop -> Bool
+isTaut p = and [ eval s p | s <- substs p ]
+
+p1 :: Prop
+p1 = And (Var 'A') (Not (Var 'A'))
+
+p2 :: Prop
+p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+
+p3 :: Prop
+p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
+
+p4 :: Prop
+p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')
+
+p5 :: Prop
+p5 = Or (Var 'A') (Not (Var 'A'))
+
+p6 :: Prop
+p6 = Equiv (And (Not (Var 'A')) (Not (Var 'B'))) (Not (Or (Var 'A') (Var 'B')))
