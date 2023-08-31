00056737d169762544c82f98ae368cfa6f6fff8d
Patrick Bucher <patrick.bucher@mailbox.org>
2023-08-31 07:30:51 +0200

    chapter 10: rework of exercise 10.2

--- programming-in-haskell/ex-ch10.hs
+++ programming-in-haskell/ex-ch10.hs
@@ -2,7 +2,7 @@
 putStr' :: String -> IO ()
 putStr' xs = sequence_ [putChar x | x <- xs]
 
--- 10.2, 10.3
+-- 10.2
 type Board = [Int]
 
 putRow :: Int -> Int -> IO ()
@@ -12,10 +12,30 @@ putRow row num = do
   putStrLn (concat (replicate num "* "))
 
 putBoard :: Board -> IO ()
-putBoard rows = sequence_ [putRow i r | (i,r) <- zip [1..] rows]
+putBoard [] = return ()
+putBoard rs = putBoardRows rs $ 1
+
+putBoardRows :: Board -> Int -> IO ()
+putBoardRows [] _ = return ()
+putBoardRows (r:rs) i = do
+  putRow i r
+  putBoardRows rs (i+1)
+  
+-- 10.3
+putBoard' :: Board -> IO ()
+putBoard' rows = sequence_ [putRow i r | (i,r) <- zip [1..] rows]
 
 -- 10.4
 adder :: IO ()
 adder = do
   putStr "How many numbers? "
   -- TODO
+
+readLine :: IO String
+readLine = do
+  c <- getChar
+  case c of
+    '\n' -> return []
+    _ -> do
+      cs <- readLine
+      return (c:cs)
