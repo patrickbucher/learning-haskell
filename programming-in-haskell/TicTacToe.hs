e73e5cb7f11694f50beea5d4c9bf705ddaa87b78
Patrick Bucher <patrick.bucher@mailbox.org>
2023-09-04 07:06:11 +0200

    tic tac toe: game tree

--- programming-in-haskell/TicTacToe.hs
+++ programming-in-haskell/TicTacToe.hs
@@ -120,3 +120,22 @@ run' g p
 
 prompt :: Player -> String
 prompt p = "Player " ++ show p ++ ", enter your move: "
+
+data Tree a = Node a [Tree a]
+  deriving Show
+
+gametree :: Grid -> Player -> Tree Grid
+gametree g p = Node g [gametree g' (next p) | g' <- moves g p]
+
+moves :: Grid -> Player -> [Grid]
+moves g p
+  | won g = []
+  | full g = []
+  | otherwise = concat [move g i p | i <- [0..((size^2)-1)]]
+
+prune :: Int -> Tree a -> Tree a
+prune 0 (Node x _) = Node x []
+prune n (Node x ts) = Node x [prune (n-1) t | t <- ts]
+
+depth :: Int
+depth = 9
