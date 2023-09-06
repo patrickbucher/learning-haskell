3f9298d0fe79c64464a035d3d4b5f2c7c45738e0
Patrick Bucher <patrick.bucher@mailbox.org>
2023-09-06 09:42:13 +0200

    finished TicTacToe, first exercise

--- programming-in-haskell/TicTacToe.hs
+++ programming-in-haskell/TicTacToe.hs
@@ -127,6 +127,12 @@ data Tree a = Node a [Tree a]
 gametree :: Grid -> Player -> Tree Grid
 gametree g p = Node g [gametree g' (next p) | g' <- moves g p]
 
+-- 11.1
+treesize :: Tree a -> Int
+treesize (Node _ ts) = 1 + sum (map treesize ts)
+-- treesize (gametree empty O)
+-- 549946
+
 moves :: Grid -> Player -> [Grid]
 moves g p
   | won g = []
@@ -139,3 +145,49 @@ prune n (Node x ts) = Node x [prune (n-1) t | t <- ts]
 
 depth :: Int
 depth = 9
+
+minimax :: Tree Grid -> Tree (Grid,Player)
+minimax (Node g [])
+  | wins O g = Node (g,O) []
+  | wins X g = Node (g,X) []
+  | otherwise = Node (g,B) []
+minimax (Node g ts)
+  | turn g == O = Node (g, minimum ps) ts'
+  | turn g == X = Node (g, maximum ps) ts'
+    where
+      ts' = map minimax ts
+      ps = [p | Node (_,p) _ <- ts']
+
+bestmove :: Grid -> Player -> Grid
+bestmove g p = head [g' | Node (g',p') _ <- ts, p' == best]
+  where
+    tree = prune depth (gametree g p)
+    Node (_,best) ts = minimax tree
+
+main :: IO ()
+main = do
+  hSetBuffering stdout NoBuffering
+  play empty O
+
+play :: Grid -> Player -> IO ()
+play g p = do
+  cls
+  goto (1,1)
+  putGrid g
+  play' g p
+
+play' :: Grid -> Player -> IO ()
+play' g p
+  | wins O g = putStrLn "Player O wins!\n"
+  | wins X g = putStrLn "Player X wins!\n"
+  | full g = putStrLn "It's a draw!\n"
+  | p == O = do
+      i <- getNat (prompt p)
+      case move g i p of
+        [] -> do
+          putStrLn "ERROR: Invalid move"
+          play' g p
+        [g'] -> play g' (next p)
+  | p == X = do
+      putStr "Player X is thinking... "
+      (play $! (bestmove g p)) (next p)
