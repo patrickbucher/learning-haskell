239c031768169ec4b0a6948c0f8fe25117e03844
Patrick Bucher <patrick.bucher@mailbox.org>
2023-09-12 10:29:51 +0200

    applicatives, monads

--- programming-in-haskell/ch12.hs
+++ programming-in-haskell/ch12.hs
@@ -10,3 +10,23 @@ sqr = fmap (^2)
 -- 2) fmap (g . h) = fmap g . fmap h
 
 -- Applicatives: Functors that allow for the application of multi-arity functions.
+
+-- Applicative Laws
+-- 1) pure id <*> x = x
+-- 2) pure (g x) = pure g <*> pure x
+-- 3) x <*> pure y = pure (\g -> g y) <*> x
+-- 4) x <*> (y <*> z) = (pure (.) <*> x <*> y) <*> z
+
+-- Monads:
+data Expr = Val Int | Div Expr Expr
+
+safediv :: Int -> Int -> Maybe Int
+safediv _ 0 = Nothing
+safediv n m = Just (n `div` m)
+
+eval :: Expr -> Maybe Int
+eval (Val n) = Just n
+eval (Div x y) = do
+  m <- eval x
+  n <- eval y
+  safediv m n
