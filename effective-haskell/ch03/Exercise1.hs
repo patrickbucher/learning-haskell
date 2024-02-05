794427cc27ac3c565e61d1f9c4bd703a10567981
Patrick Bucher <patrick.bucher@mailbox.org>
2024-02-05 07:43:07 +0100

    chapter 3: exercise 1

---
+++ effective-haskell/ch03/Exercise1.hs
@@ -0,0 +1,10 @@
+addThree :: Int -> Int -> Int -> Int
+
+-- definition 1
+-- addThree = undefined
+
+-- definition 2
+-- addThree a b c = undefined
+
+-- There are different versions because currying: a function can be written in
+-- a point-free style. Furthermore, undefined matches a lot of different types.
