fcce023b3b26eda6272fd96894ea4e4e0379818d
Patrick Bucher <patrick.bucher@mailbox.org>
2024-02-12 08:14:48 +0100

    soccer-table: added Haddock comments

--- soccer-table/src/SoccerTable.hs
+++ soccer-table/src/SoccerTable.hs
@@ -5,13 +5,25 @@ module SoccerTable
 import Text.Regex.Base
 import Text.Regex.TDFA
 
+-- |
+-- Result is a parsed soccer game end result.
 data Result = Result
   { homeTeam :: String
+  -- ^ The name of the home team.
   , awayTeam :: String
+  -- ^ The name of the away team.
   , homeGoals :: Int
+  -- ^ The amount of goals scored by the home team.
   , awayGoals :: Int
+  -- ^ The amount of goals scored by the away team.
   } deriving (Show, Eq)
 
+-- |
+-- Parses a string such as \"Foo 3:2 Bar\" to a result with the respective
+-- home/away team name and goals.
+--
+-- >>> parseResult "Foo 3:2 Bar"
+-- Result {homeTeam = "Foo", awayTeam = "Bar", homeGoals = 3, awayGoals = 2}
 parseResult :: String -> Result
 parseResult raw =
   let
