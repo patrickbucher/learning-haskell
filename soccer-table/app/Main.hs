54aabe5c54f4c3caa6efe162fd422fb136291194
Patrick Bucher <patrick.bucher@mailbox.org>
2024-02-18 17:41:33 +0100

    read results from file

--- soccer-table/app/Main.hs
+++ soccer-table/app/Main.hs
@@ -1,5 +1,21 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, World!"
+import Control.Monad
+import Data.List.Split (splitOn)
+import System.Environment (getArgs)
+import System.IO (openFile, hGetContents, IOMode(ReadMode))
 
+import SoccerTable (parseResult)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  lines <- readLines (args !! 0)
+  print $ map parseResult lines
+
+readLines :: String -> IO [String]
+readLines path = do
+  handle <- openFile path ReadMode
+  content <- hGetContents handle
+  let lines = splitOn "\n" content
+  return $ filter (\l -> l /= "") lines
