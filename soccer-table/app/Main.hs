9f4e5edfe180471a0e70ea4a9f3da0da0e83b929
Patrick Bucher <patrick.bucher@mailbox.org>
2024-02-21 08:36:26 +0100

    compute single-round table rows out of result

--- soccer-table/app/Main.hs
+++ soccer-table/app/Main.hs
@@ -3,10 +3,10 @@ module Main where
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import System.Environment (getArgs)
-import SoccerTable (parseResult)
+import SoccerTable (parseResult, toTableRows)
 
 main :: IO ()
 main = do
   [file] <- getArgs
   text <- TIO.readFile file
-  print $ take 10 $ map parseResult $ map T.unpack $ T.split (== '\n') text
+  print $ take 10 $ concat $ map toTableRows $ map parseResult $ map T.unpack $ T.split (== '\n') text
--- soccer-table/src/SoccerTable.hs
+++ soccer-table/src/SoccerTable.hs
@@ -1,6 +1,7 @@
 module SoccerTable
   ( Result (Result)
   , parseResult
+  , toTableRows
   ) where
 import Text.Regex.Base
 import Text.Regex.TDFA
@@ -19,18 +20,57 @@ data Result = Result
   } deriving (Show, Eq)
 
 -- |
--- Parses a string such as \"Foo 3:2 Bar\" to a result with the respective
--- home/away team name and goals.
+-- TableRow is a row in a League table, representing a team.
+data TableRow = TableRow
+  { name :: String
+  -- ^ The name of the team.
+  , gamesWon :: Int
+  -- ^ The number of games won.
+  , gamesLost :: Int
+  -- ^ The number of games lost.
+  , gamesTied :: Int
+  -- ^ The number of games tied.
+  , goalsScored :: Int
+  -- ^ The number of goals scored.
+  , goalsConceded :: Int
+  -- ^ The number of goals conceded.
+  , goalsDiff :: Int
+  -- ^ The difference of goalsScored and goalsConceded.
+  } deriving (Show, Eq)
+
+-- |
+-- parseResult parses a string such as \"Foo 3:2 Bar\" to a result with the
+-- respective home/away team name and goals.
 --
 -- >>> parseResult "Foo 3:2 Bar"
 -- Result {homeTeam = "Foo", awayTeam = "Bar", homeGoals = 3, awayGoals = 2}
 parseResult :: String -> Result
 parseResult raw =
   let
-    hT = matches !! 1
-    hG = read (matches !! 2) :: Int
-    aG = read (matches !! 3) :: Int
-    aT = matches !! 4
+    [_, hT, hG, aG, aT] = matches
     matches = getAllTextSubmatches (raw =~ "(.+) ([[:digit:]]+):([[:digit:]]+) (.+)")
   in
-    Result hT aT hG aG
+    Result hT aT (read hG) (read aG)
+
+-- |
+-- toTableRows computes a TableRow (representing the outcome of a single game)
+-- for each team.
+--- >>> toTableRows Result {homeTeam = "Foo", awayTeam = "Bar", homeGoals + 3, awayGoals = 2}
+-- [TableRow {name = "Foo", ...}
+--  TableRow {name = "Bar", ...}]
+toTableRows :: Result -> [TableRow]
+toTableRows r =
+  let
+    home = TableRow (homeTeam r) 0 0 0 (homeGoals r) (awayGoals r) (homeGoals r - awayGoals r)
+    away = TableRow (awayTeam r) 0 0 0 (awayGoals r) (homeGoals r) (awayGoals r - homeGoals r)
+  in
+    case compare (homeGoals r) (awayGoals r) of
+      GT ->
+        [ home {gamesWon = 1, gamesLost = 0, gamesTied = 0}
+        , away {gamesWon = 0, gamesLost = 1, gamesTied = 0} ]
+      LT ->
+        [ home {gamesWon = 0, gamesLost = 1, gamesTied = 0}
+        , away {gamesWon = 1, gamesLost = 0, gamesTied = 0} ]
+      EQ ->
+        [ home {gamesWon = 0, gamesLost = 0, gamesTied = 1}
+        , away {gamesWon = 0, gamesLost = 0, gamesTied = 1} ]
