8c94a2447f4935f8f490621a5f5966b2219f9ed0
Patrick Bucher <patrick.bucher@mailbox.org>
2024-03-03 17:52:52 +0100

    chapter 1: exercise c

---
+++ tfwh/ch01/ExerciseC.hs
@@ -0,0 +1,8 @@
+-- 'H' is of type Char
+-- "H" is of type [Char] or String
+-- 2001 is of type Int
+-- "2001" is of type [Char] or String
+
+-- [1,2,3] ++ [3,2,1] == [1,2,3,3,2,1]
+-- "Hello" ++ " World!" == "Hello World!"
+-- "HellO" ++ "" ++ "World!" == "HelloWorld!"
