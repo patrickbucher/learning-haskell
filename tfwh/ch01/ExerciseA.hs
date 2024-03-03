b8482e2e541376099e51eb58290dc33f2eedf632
Patrick Bucher <patrick.bucher@mailbox.org>
2024-03-03 12:14:55 +0100

    chapter 1: exercise a

---
+++ tfwh/ch01/ExerciseA.hs
@@ -0,0 +1,24 @@
+module ExerciseA where
+
+double :: Integer -> Integer
+double x = 2 * x
+
+-- map double [1,4,4,3]
+--[2,8,8,6]
+--
+-- map (double . double) [1,4,4,3]
+-- [4,16,16,12]
+
+-- map double []
+-- []
+
+-- sum . map double = double . sum
+-- true: law of distribution 2*(1+2+3)=(2*1+2+2+2*3)
+
+-- sum . map sum = sum . concat
+-- true: (1+2+3) + (4+5+6) = 6 + 15 = 21 = (1+2+3+4+5+6)
+-- check:
+-- sum . map sum $ [[1,2,3],[4,5,6]]
+-- 21
+-- sum . concat $ [[1,2,3],[4,5,6]] 
+-- 21
