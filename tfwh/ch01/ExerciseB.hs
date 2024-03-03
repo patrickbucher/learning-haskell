e0f090c56136a0c3bf395bbf29e17fce9f583593
Patrick Bucher <patrick.bucher@mailbox.org>
2024-03-03 17:50:50 +0100

    chapter 1: exercise b

---
+++ tfwh/ch01/ExerciseB.hs
@@ -0,0 +1,8 @@
+-- sin^2 theta
+-- wrong: the exponentiation operator cannot be applied to functions
+--
+-- sin theta^2
+-- correct: same as (sin(theta))^2
+--
+-- (sin theta)^2
+-- correct: function application has the highest precedence
